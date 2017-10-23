60b589a7f24bffb60cb4a2b246a74202b8325205
Neda Vazharova <vazharova.user@gmail.com>
2017-10-23 23:07:48 +0100

    Solved printing numbers

---
+++ HelloWorld/PrintNumbers/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace PrintNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine(1);
+            System.Console.WriteLine(11);
+            System.Console.WriteLine(1001);
+        }
+    }
+}
